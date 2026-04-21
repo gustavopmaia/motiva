fb16bc6f722544103bb948389b8df9c172822093
Gustavo Maia <gustavopmaia@yahoo.com.br>
2026-04-21 13:03:30 -0300

    fix import

--- apps/backend/src/domain/repositories/user.repository.ts
+++ apps/backend/src/domain/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { User } from "../entities/user.entity";
+import { User } from "@domain/entities/user.entity";
 
 export abstract class IUserRepository {
   abstract findByEmail(email: string): Promise<User | null>;
