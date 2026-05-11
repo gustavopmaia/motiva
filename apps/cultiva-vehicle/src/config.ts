d5f004eea41245f47d3728a9a5d6adad900b6c35
Gustavo Maia <gustavopmaia@yahoo.com.br>
2026-05-11 13:01:29 -0300

    feat: remove app

--- apps/cultiva-vehicle/src/config.ts
+++
@@ -1,3 +0,0 @@
-// Edit these before building
-export const API_URL = "http://YOUR_BACKEND_HOST:3000";
-export const API_KEY = "YOUR_API_KEY";
--- apps/cultiva-vehicle/src/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "react-native-keep-awake" {
-  import React from "react";
-  export default class KeepAwake extends React.Component {}
-  export function activate(): void;
-  export function deactivate(): void;
-}
